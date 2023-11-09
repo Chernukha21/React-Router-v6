eb6df488c568632759c040b675ea098a678598d8
Chernukha21 <Chorniy2008@ukr.net>
2023-11-09 14:18:05 +0200

    search params lesson

---
+++ src/componets/BlogFilter.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {useSearchParams} from "react-router-dom";
+
+const BlogFilter = ({searchQuery, latest, setSearchParams}) => {
+    const [search, setSearch] = useState(searchQuery);
+    const [checked, setChecked] = useState(latest);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const isLatest = form.latest.checked;
+        const query = form.search.value;
+        const params = {};
+        if(query.length) params.post = query;
+        if(isLatest) params.latest = true;
+        setSearchParams(params);
+    }
+    return (
+        <form autoComplete="off" onSubmit={handleSubmit}>
+            <input type="search" name="search" value={search} onChange={e => setSearch(e.target.value)}/>
+            <label style={{padding: '0 10px'}}>
+                <input type="checkbox" value={checked} name="latest" onChange={e => setChecked(e.target.value)}/> New only
+            </label>
+            <button type="submit">search</button>
+        </form>
+    );
+};
+
+export default BlogFilter;
--- src/pages/Blog.page.js
+++ src/pages/Blog.page.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {useState, useEffect} from "react";
-import {NavLink, useLocation} from "react-router-dom";
+import {NavLink, useLocation, useSearchParams} from "react-router-dom";
 import CustomLink from "../componets/CustomLink";
+import BlogFilter from "../componets/BlogFilter";
+
 
 
 const BlogPage = () => {
     const [posts, setPosts] = useState([]);
-    console.log(useLocation());
+    const [searchParams, setSearchParams] = useSearchParams();
 
+    const searchQuery = searchParams.get('post') || '';
+    const latest = searchParams.has('latest');
+    const startsFrom = latest ? 80 : 1;
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -19,8 +24,9 @@ const BlogPage = () => {
     return (
         <div>
             <h1>Blog Page</h1>
+            <BlogFilter latest={latest} searchQuery={searchQuery} setSearchParams={setSearchParams}/>
             <CustomLink to="/posts/new">Add new post</CustomLink>
-            {posts.map(post =>
+            {posts.filter(post => post.title.includes(searchQuery) && post.id >= startsFrom).map(post =>
                 (<NavLink to={`/posts/${post.id}`}>
                     <li>{post.title}</li>
                 </NavLink>)
